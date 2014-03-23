9bee71d29a27a564d532c8aa98c6440f5e2c4618
Yazhong Liu <l900422@vip.qq.com>
2014-03-23 20:27:23 +0800

    tweak format

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var util = require('util');
 
 function ext2type(name) {
   if (typeof name !== 'string') {
-		throw new Error('1st argument should be a string');
-	}
-	return util.format('application/%s', name.replace(/^\./, ''));
+    throw new Error('1st argument should be a string');
+  }
+  return util.format('application/%s', name.replace(/^\./, ''));
 }
 
 module.exports = ext2type;
